bbea8aac0e8073b474efe0a998a73a49ff45d63c
Brian Merchant <bhmerchant@gmail.com>
2018-08-19 11:58:28 -0700

    update

--- ash/src/vk.rs
+++ ash/src/vk.rs
@@ -4982,7 +4982,7 @@ pub struct Extent2D {
     pub height: uint32_t,
 }
 #[repr(C)]
-#[derive(Copy, Clone, Default, Debug, Hash, PartialEq)]
+#[derive(Copy, Clone, Default, Debug, Hash, PartialEq, Eq)]
 pub struct Extent3D {
     pub width: uint32_t,
     pub height: uint32_t,
